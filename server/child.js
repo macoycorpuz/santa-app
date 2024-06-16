73030103b52b7eaba66ff0491c3d3c5cb4c28312
Marcuz Corpuz <macoycorpuz@gmail.com>
2024-06-16 17:26:16 +0900

    feature: create child server and check age

--- server/child.js
+++ server/child.js
@@ -3,6 +3,10 @@ const instance = axios.create({
   baseURL: 'https://raw.githubusercontent.com/alj-devops/santa-data/master',
 });
 
+const CHILD_NOT_REGISTERED = 'Child is not registered';
+const CHILD_AGE_LIMIT = 10;
+const CHILD_AGE_LIMIT_MESSAGE = `Child should be ${CHILD_AGE_LIMIT} years old or younger`;
+
 const getChild = async (username) => {
   const { data } = await instance.get('users.json');
   const child = data.find((child) => child.username === username);
@@ -15,15 +19,40 @@ const getChildProfile = async (userUid) => {
   return childProfile;
 };
 
-const getChildAge = (birthdate) => {
+const getChildAge = (date) => {
   const today = new Date();
-  const birthDate = new Date(birthdate);
-  const age = today.getFullYear() - birthDate.getFullYear();
-  const month = today.getMonth() - birthDate.getMonth();
-  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
+  const birthdate = new Date(date);
+  if (today.getDate() < birthdate.getDate()) {
+    return -1;
+  }
+
+  const age = today.getFullYear() - birthdate.getFullYear();
+  const month = today.getMonth() - birthdate.getMonth();
+  if (month < 0) {
+    return age - 1;
   }
   return age;
 };
 
-module.exports = { getChild, getChildProfile, getChildAge };
+const sendWish = async (reqeust, response) => {
+  const { userid, wish } = reqeust.body;
+
+  const child = await getChild(userid);
+  if (!child?.uid) {
+    return response.status(404).json({ message: CHILD_NOT_REGISTERED });
+  }
+
+  const childProfile = await getChildProfile(child.uid);
+  if (!childProfile?.userUid) {
+    return response.status(404).json({ message: CHILD_NOT_REGISTERED });
+  }
+
+  const age = getChildAge(childProfile.birthdate);
+  if (age > CHILD_AGE_LIMIT) {
+    return response.status(400).json({ message: CHILD_AGE_LIMIT_MESSAGE });
+  }
+
+  response.json({ id: child.uid, isWishSent: true });
+};
+
+module.exports = { sendWish };
--- server/index.js
+++ server/index.js
@@ -2,26 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
-const { getChild, getChildProfile } = require('./child');
+const { sendWish } = require('./child');
 
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-app.post('/wish', async (req, res) => {
-  const { username, wish } = req.body;
-
-  const child = await getChild(username);
-  if (!child?.uid) {
-    return res.status(404).json({ message: 'Child not found' });
-  }
-
-  const childProfile = await getChildProfile(child.uid);
-  if (!childProfile?.userUid) {
-    return res.status(404).json({ message: 'Child not found' });
-  }
-
-  res.json({ wish, child });
-});
+app.post('/wish', sendWish);
 
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
